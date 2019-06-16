825e5ee8dfc914e0ee64584e308524b72f174b69
Anveshrithaa S <30744902+Anveshrithaa@users.noreply.github.com>
2019-06-16 20:58:48 +0530

    Add files via upload

---
+++ word-count/word-count.py
@@ -0,0 +1,25 @@
+#re- regular expression
+import re
+from pyspark import SparkConf, SparkContext
+
+#split based on what the regular expression identifies as words and change to lower case
+#and strip out things that aren't part of the words
+def normalizeWords(text):
+    return re.compile(r'\W+', re.UNICODE).split(text.lower())
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf = conf)
+
+#Load data
+input = sc.textFile("file:///sparkcourse/book.txt")
+words = input.flatMap(normalizeWords)
+
+wordCounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+wordCountsSorted = wordCounts.map(lambda x: (x[1], x[0])).sortByKey()
+results = wordCountsSorted.collect()
+
+for result in results:
+    count = str(result[0])
+    word = result[1].encode('ascii', 'ignore')
+    if (word):
+        print(word.decode() + ":\t\t" + count)
