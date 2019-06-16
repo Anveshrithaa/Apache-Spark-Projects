eba169b5989023d6919618ffa9ae0db6cdcdd92c
Anveshrithaa S <30744902+Anveshrithaa@users.noreply.github.com>
2019-06-16 23:34:41 +0530

    Add files via upload

---
+++ similar-movies/similar-movies.py
@@ -0,0 +1,102 @@
+import sys
+from pyspark import SparkConf, SparkContext
+from math import sqrt
+
+def loadMovieNames():
+    movieNames = {}
+    with open("ml-100k/u.ITEM", encoding='ascii', errors='ignore') as f:
+        for line in f:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
+
+#explicitly extract the ratings now.
+def makePairs( userRatings ):
+    ratings = userRatings[1]
+    (movie1, rating1) = ratings[0]
+    (movie2, rating2) = ratings[1]
+    return ((movie1, movie2), (rating1, rating2))
+
+def filterDuplicates( userRatings ):
+    ratings = userRatings[1]
+    (movie1, rating1) = ratings[0]
+    (movie2, rating2) = ratings[1]
+    return movie1 < movie2
+
+#function to compute similarity
+def computeCosineSimilarity(ratingPairs):
+    numPairs = 0
+    sum_xx = sum_yy = sum_xy = 0
+    for ratingX, ratingY in ratingPairs:
+        sum_xx += ratingX * ratingX
+        sum_yy += ratingY * ratingY
+        sum_xy += ratingX * ratingY
+        numPairs += 1
+
+    numerator = sum_xy
+    denominator = sqrt(sum_xx) * sqrt(sum_yy)
+
+    score = 0
+    if (denominator):
+        score = (numerator / (float(denominator)))
+
+    return (score, numPairs)
+
+# local[*] allows the use of many executors (multiple cores of the CPU) to execute the programs due to large RDD
+conf = SparkConf().setMaster("local[*]").setAppName("MovieSimilarities")
+sc = SparkContext(conf = conf)
+
+print("\nLoading movie names...")
+nameDict = loadMovieNames()
+
+data = sc.textFile("c:/SparkCourse/ml-100k/u.data")
+
+# Map ratings to key / value pairs: user ID => movie ID, rating
+ratings = data.map(lambda l: l.split()).map(lambda l: (int(l[0]), (int(l[1]), float(l[2]))))
+
+# Emit every movie rated together by the same user.
+# Self-join to find every combination.
+joinedRatings = ratings.join(ratings)
+
+# At this point our RDD consists of userID => ((movieID, rating), (movieID, rating))
+
+# Filter out duplicate pairs
+uniqueJoinedRatings = joinedRatings.filter(filterDuplicates)
+
+moviePairs = uniqueJoinedRatings.map(makePairs)
+
+# We now have (movie1, movie2) => (rating1, rating2)
+#collect all ratings for each movie pair and compute similarity
+moviePairRatings = moviePairs.groupByKey()
+
+# We now have (movie1, movie2) = > (rating1, rating2), (rating1, rating2) ...
+#compute similarities.
+moviePairSimilarities = moviePairRatings.mapValues(computeCosineSimilarity).cache()
+
+# Save the results if needed
+#moviePairSimilarities.sortByKey()
+#moviePairSimilarities.saveAsTextFile("movie-sims")
+
+# Extract similarities for the movie we care about that are "good" using the quality threshold
+if (len(sys.argv) > 1):
+
+    scoreThreshold = 0.97
+    coOccurenceThreshold = 50
+
+    movieID = int(sys.argv[1])
+
+    # Filter for movies with this sim that are "good" as defined by the quality thresholds above
+    filteredResults = moviePairSimilarities.filter(lambda pairSim: \
+        (pairSim[0][0] == movieID or pairSim[0][1] == movieID) \
+        and pairSim[1][0] > scoreThreshold and pairSim[1][1] > coOccurenceThreshold)
+
+    # Sort by quality score.
+    results = filteredResults.map(lambda pairSim: (pairSim[1], pairSim[0])).sortByKey(ascending = False).take(10)
+
+    print("Top 10 similar movies for " + nameDict[movieID])
+    for result in results:
+        (sim, pair) = result        
+        similarMovieID = pair[0]
+        if (similarMovieID == movieID):
+            similarMovieID = pair[1]
+        print(nameDict[similarMovieID] + "\tscore: " + str(sim[0]) + "\tstrength: " + str(sim[1]))
